c20eaaca8c1cab1e706dc4dcb8a132f8244a7b06
Frank Neuber <fn@kernelport.com>
2018-12-21 11:23:55 +0100

    npm install git+https://git.kernelport.com/r/npm/segment-display.git#v0.0.5 update usage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {interval} from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'segment-display-test';
+    title = 'segment-display-test';
+    digi2 = "10";
+    counter = 10;
+
+    constructor() {
+        interval(1000)
+            .subscribe(() => {
+                if (this.counter === 0)
+                    this.counter = 10;
+                else
+                    this.counter--;
+                this.digi2 = String(this.counter).padStart(2, "0");
+            });
+    }
 }
